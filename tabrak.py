bc151b46bc7891517e5f040e51430264795f239c
kisanakkkkk <kisanak@kali.kisanak.awikwok>
2025-03-01 03:51:44 +0000

    add mutation query generator and optimize variable requirement

--- tabrak.py
+++ tabrak.py
@@ -1,5 +1,5 @@
 import requests
-from graphql import build_client_schema, get_introspection_query, GraphQLObjectType
+from graphql import build_client_schema, get_introspection_query, GraphQLObjectType, GraphQLInputObjectType
 import json
 import yaml
 from http.cookies import SimpleCookie
@@ -86,32 +86,94 @@ def generate_query_string(query_name, query_info):
             query = f"{{ {query_name} }}"
     return query, variables, global_arg_str
 
+def generate_mutation_string(mutation_name, mutation_info):
+    global_arg_list = []
+    args_list = []
+    variables = {}
+
+    for arg in mutation_info.args:
+        global_arg_list.append(f'${arg}: {mutation_info.args[arg].type}')
+        variables[arg] = config[str(arg)]
+        args_list.append(f'{arg}: ${arg}')
+
+    global_arg_str = ", ".join(global_arg_list)
+    args_str = ", ".join(args_list)
+    vars_str = ", ".join([f"{arg.name}: ${arg.name}" for arg in mutation_info.args if hasattr(arg, 'type')])
+
+    return_type = get_full_type(mutation_info.type)
+    field_string = generate_field_string(return_type, 0)
+    if field_string:
+        if args_str:
+            mutation = f"mutation ({global_arg_str}) {{ {mutation_name}({args_str}) {{ {field_string} }} }} "
+        else:
+            mutation = f"{{ {mutation_name} {{ {field_string} }} }}"
+    else:
+        if args_str:
+            mutation = f"mutation ({global_arg_str}) {{ {mutation_name}({args_str}) }}"
+        else:
+            mutation = f"{{ {mutation_name} }}"
+    return mutation, variables, global_arg_str
+
+
 def send_query(query, variables, url, headers=None):
     """ Send the query to the GraphQL API and return the response. """
-    response = requests.post(url, json={'query': query, 'variables': variables}, headers=headers, cookies=json_cookies)
+    jsons = {'query': query, 'variables': variables}
+    print(bcolors.BOLD + "Request Body (copy to burpsuite):" + bcolors.ENDC)
+    print(f'{json.dumps(jsons)}')
+    print("")
+    response = requests.post(url, json=jsons, headers=headers, cookies=json_cookies)
     response.raise_for_status()
     return response.json()
 
-def get_var_requirements(queries):
+def get_var_requirements(operations, schema):
+    def get_input_fields(input_type, depth, max_depth=config['max_depth']):
+        if depth > max_depth:
+            return ""  # Prevents deep nesting beyond max_depth
+        """Recursively retrieves input object fields."""
+        fields = []
+        if hasattr(input_type, 'fields'):
+            for field_name, field in input_type.fields.items():
+                field_type = get_full_type(field.type)
+                # print(field_type, type(field_type))
+                # print(isinstance(field_type, GraphQLInputObjectType))
+                if isinstance(field_type, GraphQLInputObjectType):
+                    sub_fields = get_input_fields(field_type, depth+1, max_depth)
+                    fields.append(f"{field_name}: {{ {', '.join(sub_fields)} }}")
+                else:
+                    fields.append(f"{field_name}: {field_type}")
+        return fields
+
     var_reqs = {}
-    for query_name, query_info in queries.items():
-        for arg in query_info.args:
-            var_reqs[arg] = query_info.args[arg].type
+    
+    for operation_name, operation_info in operations.items():
+        for arg in operation_info.args:
+            arg_type = get_full_type(operation_info.args[arg].type)
+            input_fields = get_input_fields(arg_type, 1)
+            var_reqs[arg] = arg_type.name + " { " + ", ".join(input_fields) + " }"
+
     sorted_dict = dict(sorted(var_reqs.items()))
-    for var in sorted_dict:
-        print(f'{var}: {sorted_dict[var]}')
+    
+    for var, type_desc in sorted_dict.items():
+        print(f'{var}: {type_desc}')
+
 
 def generate_and_send_queries(schema, url, headers=None, cookies=None):
     query_type = schema.query_type
     queries = query_type.fields
+
+    mutation_type = schema.mutation_type
+    mutations = mutation_type.fields
+    
     print("1. see all args requirements")
-    print("2. generate queries")
+    print("2. generate and send queries")
+    print("3. generate mutations (mutations is dangerous so we are not gonna send it)")
     opt = input(">> ")
     if opt == "1":
+        print("\n" + bcolors.BOLD + "Queries Variables Required:" + bcolors.ENDC + "\n")
+        get_var_requirements(queries, schema)
         print("")
-        print(bcolors.BOLD + "Variables Required:" + bcolors.ENDC)
-        print("")
-        get_var_requirements(queries)
+        print("\n" + bcolors.BOLD + "Mutations Variables Required:" + bcolors.ENDC + "\n")
+        get_var_requirements(mutations, schema)
     elif opt == "2":
         for query_name, query_info in queries.items():
             print(bcolors.OKBLUE + bcolors.BOLD + f"Processing Query: {query_name}" + bcolors.ENDC)
@@ -120,9 +182,9 @@ def generate_and_send_queries(schema, url, headers=None, cookies=None):
             print(bcolors.BOLD + "Variables Required:" + bcolors.ENDC)
             print(f'{global_arg_str}')
             print("")
-            print(bcolors.BOLD + "Generated Query String:" + bcolors.ENDC)
-            print(f"{query_string}")
-            print("")
+            # print(bcolors.BOLD + "Generated Query String:" + bcolors.ENDC)
+            # print(f"{query_string}")
+            # print("")
             print(bcolors.BOLD + "Variables supplied:" + bcolors.ENDC)
             print(f"{json.dumps(variables)}")
             print("")
@@ -137,6 +199,25 @@ def generate_and_send_queries(schema, url, headers=None, cookies=None):
                 print(f"An error occurred while processing {query_name}: {err}")
             
             print("")  # Newline for better readability
+    elif opt == "3":
+        for mutation_name, mutation_info in mutations.items():
+            print(mutation_name, mutation_info)
+            print(bcolors.OKBLUE + bcolors.BOLD + f"Processing Query: {mutation_name}" + bcolors.ENDC)
+            print("")
+            mutation_string, variables, global_arg_str = generate_mutation_string(mutation_name, mutation_info)
+            print(bcolors.BOLD + "Variables Required:" + bcolors.ENDC)
+            print(f'{global_arg_str}')
+            print("")
+            print(bcolors.BOLD + "Generated Query String:" + bcolors.ENDC)
+            print(f"{mutation_string}")
+            print("")
+            print(bcolors.BOLD + "Variables supplied:" + bcolors.ENDC)
+            print(f"{json.dumps(variables)}")
+            print("")
+            jsons = {'query': mutation_string, 'variables': variables}
+            print(bcolors.BOLD + "Request Body (copy to burpsuite):" + bcolors.ENDC)
+            print(f'{json.dumps(jsons)}')
+            print("")
     else:
         print("[!] Error")
 
